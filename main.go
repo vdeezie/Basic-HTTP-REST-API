99fa406b2d440c01925435247723c9a9bc89cba1
Monsieur Victor Dèsire <edesire22@yahoo.com>
2020-05-09 20:46:37 +0100

    commit

--- main.go
+++ main.go
@@ -2,36 +2,13 @@ package main
 
 import (
 	"fmt"
-    "database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	
+	"net/http"	
 )
---
--- Table structure for table `employee`
---
-
-CREATE TABLE IF NOT EXISTS `employee` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key',
-  `employee_name` varchar(255) NOT NULL COMMENT 'employee name',
-  `employee_salary` double NOT NULL COMMENT 'employee salary',
-  `employee_age` int(11) NOT NULL COMMENT 'employee age',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='datatable demo table' AUTO_INCREMENT=158 ;
-
 
 func main() {
-	fmt.Println("Book Service")
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, Go HTTP")
+	})
 
-	db, err := sql.Open("mysql", "root:595983Fc@tcp(127.0.0.1:3306)/testdb")
-
-	if err != nil {
-		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
-	}
-	defer db.Close()
-	
-	// Open doesn't open a connection. Validate DSN data:
-	err = db.Ping()
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
+	http.ListenAndServe(":8000", nil)
 }
