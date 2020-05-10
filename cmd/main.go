3cef5ec5d25ca2a37f698cd7d7700ba02d6790fb
Monsieur Victor Dèsire <edesire22@yahoo.com>
2020-05-10 01:22:29 +0100

    commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"net/http"	
+	"html/template"
+	"encoding/json"
+	//"fmt"
+	"net/http"
+	"time"	
 )
 
-type Book struct
+var books []Book
+
+//Book model
+type Book struct {
 	Name		string	`json: "name"`
 	Author		string	`json: "author"`
 	PublishedAt	string	`json: "published_at"`
@@ -13,8 +19,6 @@ type Book struct
 }
 
 func loadBooks(w http.ResponseWriter, r *http.Request) {
-	book := Book{"The Silmarilion", "JRR Tolkein", time.Now().Local().String()}
-
 	bk, err :=json.Marshal(book)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -24,6 +28,11 @@ func loadBooks(w http.ResponseWriter, r *http.Request) {
 	w.Write(bk)
 }
 
+func create(w http.ResponseWriter, r*http.Request) {
+	tmpl, _ := template.ParseFile("./assets/form.html")
+	tmpl.Execute(w, nil)
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/books", loadBooks)
@@ -31,44 +40,6 @@ func main() {
 	fs := http.FileServer(http.Dir("assets/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fs))
 	
+	mux.HandleFunc("/new", create)
 	http.ListenAndServe(":8000", mux)
 }
-/*type books map [string]string
-
-func (b books) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/home":
-		fmt.Fprintf(w, "Welcome to book server. You came via %s\n", r.URL.String())
-	case "/about":
-		fmt.Fprintf(w, "All about our library of books. You came via %s\n", r.URL.String())
-	case "/book":
-		item := r.URL.Query().Get("item")
-		book, ok := b[item]
-		if !ok {
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprintf(w, "book not found for: %q\n", item)
-			return
-		}
-		fmt.Fprintf(w, "The book is %s\n", book)
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "url not found! %s\n", r.URL.String())
-	}
-	
-}
-
-func main() {
-	//http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-	//	fmt.Fprint(w, "Hello, Go HTTP\n")
-	//})
-
-	//http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-	//	fmt.Fprint(w, "About, HTTP in Go\n")
-	//})
-
-	// Routing
-	bk := books{}
-	bk ["jrrtolkein"] = "The Silmarilion"
-	fmt.Println("server listening on port 8000")
-	//http.ListenAndServe(":8000", bk)
-}*/
